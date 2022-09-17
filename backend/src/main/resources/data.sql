af415002b2472b22518b60c9c846e73fce80638b
cleiton-cfdbc <cleiton.cfdbc@gmail.com>
2022-09-17 13:13:47 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('PAULO DE TARSO E SILVA','233.820.730-05','2300.00','1978-06-15','2');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('CAMILLA DOS SANTOS MAIA','490.164.510-21','2400.10','1989-10-27','3');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('GEORGINA PAULA DE FREITAS','302.497.140-86','3200.00','1985-07-23','1');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('PEDRO DE OLIVEIRA GUMARAES','077.068.600-19','4750.80','1980-03-25','3');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('MARIA QUITERIA SANTANA SANTOS','311.188.970-05','3700.70','1981-05-24','3');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('ROSANGELA DOS SANTOS GOMES','264.890.800-52','2581.20','1985-09-19','2');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('GABRIELLA RODRIGUES DE SA','229.160.990-42','2650.00','2000-08-17','0');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('ANNA GOMES OLIVEIRA SANTOS','956.722.170-79','2900.00','1993-11-20','2');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('SOLON MARCONNI DE MESQUITA','053.988.820-66','3990.30','1976-12-30','4');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('ISABELLA ROSA DA TRINDADE GOMES','183.151.520-25','3530.58','1983-11-25','2');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('PATRICIA DE ALCANTARA','320.626.960-19','2900.00','1989-03-17','3');
+INSERT INTO tb_client(name, cpf, income,birth_Date, children) VALUES ('SANDRO JUNQUEIRA GONTIJO SANTOS','544.744.100-50','3200.90','1988-08-23','3')
